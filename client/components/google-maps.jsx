0824b2daca26f5d9a1eca53ac464e4402afe3bfb
Matthew Chau <matthewwchau@gmail.com>
2023-03-04 07:51:56 +0000

    Remove all commented out code. #1

--- client/components/google-maps.jsx
+++ client/components/google-maps.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-// import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-// CODE ABOVE HAS A MARKER, WHICH YOU CAN MARK A LOCATION ON MAP WHEN RENDERED
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 export default function Gmaps(props) {
 
   const { isLoaded } = useLoadScript({
-    // googleMapsApiKey: 'process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY'
     googleMapsApiKey: 'AIzaSyAg1cYuBaHKWF7k2z-9RaVdKIrLW7tp8Kg'
   });
 
   if (!isLoaded) return <div>Loading...</div>;
   return <Map />;
-
-  // return (
-  //   <div className='d-flex justify-content-center align-items-center'>
-  //     <p>MAPS</p>
-  //   </div>
-  // );
 
   function Map() {
     return (
