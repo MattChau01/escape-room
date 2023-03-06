8387ae337d9cb993c248e73073efe03b7d181acc
Matthew Chau <matthewwchau@gmail.com>
2023-03-06 06:06:53 +0000

    Import an accordion from Material UI. #6

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/header';
 import SearchBar from '../components/search-bar';
 import Gmaps from '../components/google-maps';
+import HomeAccordion from '../components/accordion';
 
 export default function Home(props) {
   return (
@@ -11,6 +12,7 @@ export default function Home(props) {
       <Header />
       <SearchBar />
       <Gmaps />
+      <HomeAccordion />
     </div>
   );
 }
