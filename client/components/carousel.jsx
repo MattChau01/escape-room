306691fff7bda32c75132744c3c280afc90b706f
Matthew Chau <matthewwchau@gmail.com>
2023-03-08 23:51:54 +0000

    Remove all commented out code. #7

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-// import React, { useEffect, useState } from 'react';
-// import imgOne from '../../server/public/room-images/img-1.png';
-// import imgTwo from '../../server/public/room-images/img-2.png';
-// import imgThree from '../../server/public/room-images/img-3.png';
-// import imgFour from '../../server/public/room-images/img-4.png';
-// import imgFive from '../../server/public/room-images/img-5.png';
-
-// const images = [
-//   { id: 0, img: imgOne },
-//   { id: 1, img: imgTwo },
-//   { id: 2, img: imgThree },
-//   { id: 3, img: imgFour },
-//   { id: 4, img: imgFive }
-// ];
 
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // pageLoaded: false,
       count: 0
     };
     this.autoScroll = this.autoScroll.bind(this);
@@ -27,8 +12,6 @@ export default class Carousel extends React.Component {
   }
 
   componentDidMount() {
-    // console.log('loaded');
-    // this.setState({ pageLoaded: true });
     this.autoScroll();
   }
 
@@ -39,7 +22,7 @@ export default class Carousel extends React.Component {
       } else {
         this.setState({ count: this.state.count + 1 });
       }
-    }, 2000);
+    }, 3000);
   }
 
   prevImg() {
@@ -67,26 +50,22 @@ export default class Carousel extends React.Component {
   render() {
     return (
       <div className='mt-3 d-flex justify-content-center align-items-center'>
-        {/* <img src={images[0].img} alt='imageOne' style={{ width: '180px' }}/> */}
         <div className='row justify-between'>
           <div >
             <div>
-              <div className='left-arrow' onClick={this.prevImg} >
+              <button className='carousel-arrows' onClick={this.prevImg} >
                 &lt;
-              </div>
+              </button>
             </div>
           </div>
           <div className='home-carousel'>
             <img src={this.props.images[this.state.count].img} className='home-carousel-img' />
-            {/* {images.map(image =>
-            <img key={image.id} src={image.img} className='home-carousel-img' />
-          )} */}
           </div>
           <div>
             <div>
-              <div className='right-arrow' onClick={this.nextImg}>
+              <button className='carousel-arrows' onClick={this.nextImg}>
                 &gt;
-              </div>
+              </button>
             </div>
           </div>
         </div>
@@ -94,94 +73,3 @@ export default class Carousel extends React.Component {
     );
   }
 }
-
-// export default function Carousel(props) {
-//   // console.log('images: ', images[0].img);
-
-//   const [count, setCount] = useState(0);
-
-//   // useEffect(() => {
-//   //   setInterval(setCount(+1), 1000);
-//   // }, [count]);
-
-//   function autoScroll() {
-//     const timer = setInterval(() => {
-//       if (count === 4) {
-//         setCount(0);
-//       } else {
-//         setCount(count + 1);
-//       }
-//     }, 2000);
-//   }
-
-//   // useEffect(() => {
-//   //   setInterval(() => {
-//   //     if (count === 4) {
-//   //       setCount(0);
-//   //     } else {
-//   //       setCount(count + 1);
-//   //     }
-//   //   }, 5000);
-//   // }, [count]);
-
-//   useEffect(() => {
-//     autoScroll();
-//   });
-
-//   function prevImg(event) {
-//     console.log('count: ', count);
-//     event.preventDefault();
-//     clearInterval(autoScroll);
-
-//     if (count === 0) {
-//       setCount(4);
-//     } else if (count > 0) {
-//       setCount(count - 1);
-//     }
-//     autoScroll();
-//   }
-
-//   function nextImg(event) {
-//     console.log('count: ', count);
-//     event.preventDefault();
-//     clearInterval(autoScroll);
-
-//     if (count === 4) {
-//       setCount(0);
-//     } else if (count < 4) {
-//       setCount(count + 1);
-//     }
-//     autoScroll();
-//   }
-
-//   console.log('count: ', count);
-
-//   return (
-//     <div className='mt-3 d-flex justify-content-center align-items-center'>
-//       {/* <img src={images[0].img} alt='imageOne' style={{ width: '180px' }}/> */}
-//       <div className='row'>
-//         <div >
-//           <div className='left-arrow'>
-//             <div style={{ fontWeight: 600, fontSize: '20px', border: 'none', cursor: 'pointer' }} onClick={prevImg} >
-//               &lt;
-//             </div>
-//           </div>
-//         </div>
-//         <div className='home-carousel'>
-//           {/* TEST: MAPPING THRU ARRAY OF IMAGES */}
-//           <img src={images[count].img} className='home-carousel-img' />
-//           {/* {images.map(image =>
-//             <img key={image.id} src={image.img} className='home-carousel-img' />
-//           )} */}
-//         </div>
-//         <div>
-//           <div className='right-arrow'>
-//             <div style={{ fontWeight: 600, fontSize: '20px', border: 'none', cursor: 'pointer' }} onClick={nextImg}>
-//               &gt;
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Header from '../components/header';
 import SearchBar from '../components/search-bar';
-// import Gmaps from '../components/google-maps';
 import HomeAccordion from '../components/accordion';
 import Carousel from '../components/carousel';
-
-// TEST
 import imgOne from '../../server/public/room-images/img-1.png';
 import imgTwo from '../../server/public/room-images/img-2.png';
 import imgThree from '../../server/public/room-images/img-3.png';
@@ -25,7 +22,6 @@ export default function Home(props) {
     <div>
       <Header />
       <SearchBar />
-      {/* <Gmaps /> */}
       <Carousel images={images}/>
       <HomeAccordion />
     </div>
