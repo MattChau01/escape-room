3ba2c219dbaecb7fbaa5c8aee05658785405de4d
Matthew Chau <matthewwchau@gmail.com>
2023-03-29 01:39:24 +0000

    In the App component, add a function that will render pages by hash routes. #13

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import Home from './pages/home';
+import ParseRoute from '../client/lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: ParseRoute(window.location.hash)
+    };
+    this.currentPage = this.currentPage.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: ParseRoute(window.location.hash)
+      });
+    });
+  }
+
+  currentPage() {
+    const { path } = this.state.route;
+    if (path === '') {
+      return (
+        <Home />
+      );
+    }
+  }
+
   render() {
-    return <Home />;
+    // return <Home />;
+
+    return (
+      <div>
+        {this.currentPage()}
+      </div>
+    );
   }
 }
