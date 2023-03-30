0cc6f17080b63fd2cff551efbdb2c09e98e93ba0
Matthew Chau <matthewwchau@gmail.com>
2023-03-30 19:06:01 +0000

    Update server/index.js to make a post request for a new vendor signup. #13

--- server/index.js
+++ server/index.js
@@ -2,13 +2,48 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const ClientError = require('./client-error');
+
+const pg = require('pg');
+const db = new pg.Pool({
+  connectionString: `${process.env.DATABASE_URL}`,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
+const jsonMiddleWare = express.json();
 
 app.use(staticMiddleware);
+app.use(jsonMiddleWare);
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+// POST REQUEST
+app.post('/api/vendorAccount', (req, res, next) => {
+  const { firstName, lastName, username, password } = req.body;
+
+  if (!firstName || !lastName || !username || !password) {
+    throw new ClientError(400, 'Invalid Inputs');
+  } else {
+
+    const sql = `
+      insert into "vendorAccount" ("firstName", "lastName", "username", "hashedPassword")
+      values ($1, $2, $3, $4)
+    `;
+
+    const params = [firstName, lastName, username, password];
+
+    db.query(sql, params)
+      .then(result => {
+        res.status(200).json(result[0]);
+      })
+      .catch(err => next(err));
+  }
+
 });
 
 app.use(errorMiddleware);
