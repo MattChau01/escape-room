c428c86aa74621d24939e66c6630a3bf811a8fc1
Matthew Chau <matthewwchau@gmail.com>
2023-03-30 21:52:35 +0000

    Display a message that sign up was a success and buttons to reroute to home page or log in. #13

--- client/pages/vendor-success.jsx
+++ client/pages/vendor-success.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import Header from '../components/header';
 
 export default function VSuccess(props) {
+
+  function returnHome() {
+    window.location.hash = '#';
+  }
+
   return (
     <div>
-      SUCCESS PAGE
+      <Header />
+      <div className='d-flex flex-column mt-5'>
+        <div className='row justify-content-center'>
+          <h2>
+            Registration complete!
+          </h2>
+        </div>
+        <div className='row justify-content-center mt-5'>
+          <h5>
+            To sign in, click here:
+          </h5>
+        </div>
+        <div className='row justify-content-center'>
+          <a style={{ cursor: 'pointer' }}>
+            Vendor sign in
+          </a>
+        </div>
+
+        <div className='row justify-content-center mt-5'>
+          <h5 className='text-center'>
+            Click here to return to home page:
+          </h5>
+        </div>
+        <div className='row justify-content-center'>
+          <a style={{ cursor: 'pointer' }} onClick={returnHome} >
+            Home page
+          </a>
+        </div>
+
+      </div>
     </div>
   );
 }
