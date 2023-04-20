f9597de0febde6209766810422de27cec8d945fe
Matthew Chau <matthewwchau@gmail.com>
2023-04-20 01:29:30 +0000

    Define a function authorizationMiddleware to check for a token. #18

--- server/authorizationMiddleware.js
+++ server/authorizationMiddleware.js
@@ -1,2 +1,19 @@
-const jwt = require('jsonwebtoken'); // eslint-disable-line
-const ClientError = require('./client-error'); // eslint-disable-line
+const jwt = require('jsonwebtoken');
+const ClientError = require('./client-error');
+
+function authorizationMiddleware(req, res, next) {
+  try {
+    const accessToken = req.get('X-Access-Token');
+    if (!accessToken) {
+      throw new ClientError(401, 'Authentication requried');
+    } else {
+      const payload = jwt.verify(accessToken, process.env.TOKEN_SECRET);
+      req.user = payload;
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = authorizationMiddleware;
