7c9eb17b4f5658ad44c483f05b14e6b2cf87fe9e
Matthew Chau <matthewwchau@gmail.com>
2023-04-28 22:27:29 +0000

    Create a new page view catalog.jsx. #20

---
+++ client/pages/catalog.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class CatalogPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
+
+  render() {
+    return (
+      <div className='text-center'>
+        TEST
+      </div>
+    );
+  }
+}
