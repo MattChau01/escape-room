d736f928184797884c7b2e32cd983a5481ae1a2a
Matthew Chau <matthewwchau@gmail.com>
2023-05-04 05:51:44 +0000

    Move form from vendor-home to new-listing-form.jsx. #25

--- client/components/new-listing-form.jsx
+++ client/components/new-listing-form.jsx
@@ -2,8 +2,93 @@ import React from 'react';
 
 export default function ListingForm(props) {
   return (
-    <div>
-      NEW LISTING FORM
+    <div className='mt-5'>
+      <div className='text-right mr-5 v-signout'>
+        <button style={{
+          backgroundColor: '#1976D2',
+          color: '#fff',
+          borderRadius: '5rem',
+          width: '7rem',
+          textAlign: 'center',
+          paddingTop: '.25rem',
+          cursor: 'pointer',
+          outline: 'none',
+          borderStyle: 'none'
+        }} onClick={props.handleSignOut}>Sign out</button>
+      </div>
+      <div className='text-center mt-3'>
+
+        <div className='d-flex justify-content-center text-center'>
+          <p style={{
+            fontSize: '1.4rem'
+          }}>
+            Create a new listing here:
+          </p>
+        </div>
+
+        <div>
+          <div className='d-flex justify-content-center mt-3 mb-3'>
+            <form autoComplete='off' className='new-listing' onSubmit={props.handleSubmit}>
+              <label className='d-flex justify-content-center mt-3'
+                style={{
+                  fontSize: '1.25rem',
+                  fontWeight: 600,
+                  color: '#5A5A5A'
+                }}>
+                New listing
+              </label>
+
+              <div>
+
+                <label htmlFor='roomName'>
+                  <input className='new-listing-input' name='roomName' id='roomName' type='text' placeholder='Room name' value={props.roomName} onChange={props.roomNameChange} />
+                </label>
+
+                <label htmlFor='imageUrl'>
+                  <input className='new-listing-input' name='imageUrl' id='imageUrl' type='text' placeholder='Image link' value={props.imageUrl} onChange={props.imageUrlChange} />
+                </label>
+
+                <label htmlFor='address'>
+                  <input className='new-listing-input' name='address' id='address' type='text' placeholder='Address' value={props.address} onChange={props.addressChange} />
+                </label>
+
+                <label htmlFor='price'>
+                  <input className='new-listing-input' name='price' id='price' type='text' placeholder='Price' value={props.price} onChange={props.priceChange} />
+                </label>
+
+                <label htmlFor='minimumPlayers'>
+                  <input className='new-listing-input' name='minimumPlayers' id='minimumPlayers' type='text' placeholder='Minimum players' value={props.minimumPlayers} onChange={props.minimumPlayersChange} />
+                </label>
+
+                <label htmlFor='difficulty'>
+                  <input className='new-listing-input' name='difficulty' id='difficulty' type='text' placeholder='Difficulty' value={props.difficulty} onChange={props.difficultyChange} />
+                </label>
+
+                <label htmlFor='timeLimit'>
+                  <input className='new-listing-input' name='timeLimit' id='timeLimit' type='text' placeholder='Time limit' value={props.timeLimit} onChange={props.timeLimitChange} />
+                </label>
+
+                <label htmlFor='phoneNumber'>
+                  <input className='new-listing-input' name='phoneNumber' id='phoneNumber' type='text' placeholder='Phone number' value={props.phoneNumber} onChange={props.phoneNumberChange} />
+                </label>
+
+                <label htmlFor='description'>
+                  <textarea className='new-listin-desc' name='description' id='description' placeholder='Description' value={props.description} onChange={props.descriptionChange} />
+                </label>
+
+                <div className='mb-4'>
+                  <button className='new-listing-submit' type='submit' style={{
+                    cursor: 'pointer'
+                  }}>Submit</button>
+                </div>
+
+              </div>
+
+            </form>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- client/pages/vendor-home.jsx
+++ client/pages/vendor-home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
+import ListingForm from '../components/new-listing-form';
 
 export default class VendorHome extends React.Component {
   constructor(props) {
@@ -150,95 +151,33 @@ export default class VendorHome extends React.Component {
 
     return (
       <>
-        <Header participants={this.props.participants} routeVSignin={this.props.routeVSignin} toHome={this.props.toHome} />
-        <div className='mt-5'>
-          <div className='text-right mr-5 v-signout'>
-            <button style={{
-              backgroundColor: '#1976D2',
-              color: '#fff',
-              borderRadius: '5rem',
-              width: '7rem',
-              textAlign: 'center',
-              paddingTop: '.25rem',
-              cursor: 'pointer',
-              outline: 'none',
-              borderStyle: 'none'
-            }} onClick={this.props.handleSignOut}>Sign out</button>
-          </div>
-          <div className='text-center mt-3'>
-
-            <div className='d-flex justify-content-center text-center'>
-              <p style={{
-                fontSize: '1.4rem'
-              }}>
-                Create a new listing here:
-              </p>
-            </div>
-
-            <div>
-              <div className='d-flex justify-content-center mt-3 mb-3'>
-                <form autoComplete='off' className='new-listing' onSubmit={this.handleSubmit}>
-                  <label className='d-flex justify-content-center mt-3'
-                    style={{
-                      fontSize: '1.25rem',
-                      fontWeight: 600,
-                      color: '#5A5A5A'
-                    }}>
-                    New listing
-                  </label>
-
-                  <div>
-
-                    <label htmlFor='roomName'>
-                      <input className='new-listing-input' name='roomName' id='roomName' type='text' placeholder='Room name' value={this.state.roomName} onChange={this.roomName} />
-                    </label>
-
-                    <label htmlFor='imageUrl'>
-                      <input className='new-listing-input' name='imageUrl' id='imageUrl' type='text' placeholder='Image link' value={this.state.imageUrl} onChange={this.imageLink}/>
-                    </label>
-
-                    <label htmlFor='address'>
-                      <input className='new-listing-input' name='address' id='address' type='text' placeholder='Address' value={this.state.address} onChange={this.address} />
-                    </label>
-
-                    <label htmlFor='price'>
-                      <input className='new-listing-input' name='price' id='price' type='text' placeholder='Price' value={this.state.price} onChange={this.price} />
-                    </label>
-
-                    <label htmlFor='minimumPlayers'>
-                      <input className='new-listing-input' name='minimumPlayers' id='minimumPlayers' type='text' placeholder='Minimum players' value={this.state.minimumPlayers} onChange={this.minimumPlayers} />
-                    </label>
-
-                    <label htmlFor='difficulty'>
-                      <input className='new-listing-input' name='difficulty' id='difficulty' type='text' placeholder='Difficulty' value={this.state.difficulty} onChange={this.difficulty}/>
-                    </label>
-
-                    <label htmlFor='timeLimit'>
-                      <input className='new-listing-input' name='timeLimit' id='timeLimit' type='text' placeholder='Time limit' value={this.state.timeLimit} onChange={this.timeLimit}/>
-                    </label>
-
-                    <label htmlFor='phoneNumber'>
-                      <input className='new-listing-input' name='phoneNumber' id='phoneNumber' type='text' placeholder='Phone number' value={this.state.phoneNumber} onChange={this.phoneNumber} />
-                    </label>
-
-                    <label htmlFor='description'>
-                      <textarea className='new-listin-desc' name='description' id='description' placeholder='Description' value={this.state.description} onChange={this.description}/>
-                    </label>
-
-                    <div className='mb-4'>
-                      <button className='new-listing-submit' type='submit' style={{
-                        cursor: 'pointer'
-                      }}>Submit</button>
-                    </div>
-
-                  </div>
-
-                </form>
-              </div>
-            </div>
-
-          </div>
+        <Header
+          participants={this.props.participants}
+          routeVSignin={this.props.routeVSignin}
+          toHome={this.props.toHome} />
+        <div>
+          VENDOR HOME SCREEN HERE
         </div>
+        <ListingForm
+          handleSubmit={this.handleSubmit}
+          roomName={this.state.roomName}
+          roomNameChange={this.roomName}
+          imageUrl={this.state.imageUrl}
+          imageUrlChange={this.imageLink}
+          address={this.state.address}
+          addressChange={this.address}
+          price={this.state.price}
+          priceChange={this.price}
+          minimumPlayers={this.state.minimumPlayers}
+          minimumPlayersChange={this.minimumPlayers}
+          difficulty={this.state.difficulty}
+          difficultyChange={this.difficulty}
+          timeLimit={this.state.timeLimit}
+          timeLimitChange={this.timeLimit}
+          phoneNumber={this.state.phoneNumber}
+          phoneNumberChange={this.phoneNumber}
+          description={this.state.description}
+          descriptionChange={this.description}/>
       </>
     );
   }
