cb63ef098abbe225d9ad43f9a94e9c54bc3cf69d
Matthew Chau <matthewwchau@gmail.com>
2023-05-08 20:39:09 +0000

    Add the React Hook: useEffect, call an anonymous function that will call a GET request using the userId from local storage as an argument. #26

--- client/components/vendor-listings.jsx
+++ client/components/vendor-listings.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function VendorListings(props) {
+
+  useEffect(() => {
+    // console.log('useEffect here');
+    console.log('user: ', window.localStorage.getItem('userId')); //eslint-disable-line
+
+    const reqObj = {};
+    reqObj.userId = window.localStorage.getItem('userId');
+
+    const req = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(reqObj)
+    };
+
+    fetch('/api/listings/vendor', req)
+      .then(res => res.json())
+      .then(listings => {
+        console.log('listings: ', listings); //eslint-disable-line
+      })
+      .catch(err => console.error(err));
+
+  });
+
   return (
     <div>
       LIST OF POSTINGS
