fcd8fd4314c42606262ff6a80d99879626438bf5
Matthew Chau <matthewwchau@gmail.com>
2023-07-05 05:52:22 +0000

    Added in form template. Will work on updating fields with clicks. #21

--- client/pages/vendor-home.jsx
+++ client/pages/vendor-home.jsx
@@ -240,11 +240,22 @@ export default class VendorHome extends React.Component {
             : (
               <div>
 
-                <div className='text-center mt-5' style={{
-                  fontSize: '1.4rem'
-                }}>
-                  Your listings here:
-                </div>
+                {
+                  (this.state.editClicked === false)
+                    ? (
+                      <div className='text-center mt-5' style={{
+                        fontSize: '1.4rem'
+                      }}>
+                        Your listings here:
+                      </div>
+                      )
+                    : (
+                      <>
+                      &nbsp;
+                      </>
+                      )
+                }
+
                 <div className='text-center mt-2'>
                   {/* **********DO NOT DELETE - TESTING CONDITIONAL RENDER********** */}
                   {/* <VendorListings
@@ -274,8 +285,153 @@ export default class VendorHome extends React.Component {
                           <div>
                             <button onClick={this.closeEdit}>Close</button>
                           </div>
+
+                          {/* EDIT FORM BELOW */}
                           <div>
                             EDIT FORM HERE
+                            <br />
+                            <div>
+                              <div>
+                                <div className='d-flex justify-content-center mt-3 mb-3'>
+                                  <form
+                                    autoComplete='off'
+                                    className='new-listing'
+                                    // onSubmit={props.handleSubmit}
+                                    >
+                                    <label className='d-flex justify-content-center mt-3'
+                                      style={{
+                                        fontSize: '1.25rem',
+                                        fontWeight: 600,
+                                        color: '#5A5A5A'
+                                      }}>
+                                      Edit listing
+                                    </label>
+
+                                    <div>
+
+                                      <label htmlFor='roomName'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='roomName'
+                                          id='roomName'
+                                          type='text'
+                                          placeholder='Room name'
+                                          // value={props.roomName}
+                                          // onChange={props.roomNameChange}
+                                          />
+                                      </label>
+
+                                      <label htmlFor='imageUrl'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='imageUrl'
+                                          id='imageUrl'
+                                          type='text'
+                                          placeholder='Image link'
+                                          // value={props.imageUrl}
+                                          // onChange={props.imageUrlChange}
+                                          />
+                                      </label>
+
+                                      <label htmlFor='address'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='address'
+                                          id='address'
+                                          type='text'
+                                          placeholder='Address'
+                                          // value={props.address}
+                                          // onChange={props.addressChange}
+                                          />
+                                      </label>
+
+                                      <label htmlFor='price'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='price'
+                                          id='price'
+                                          type='text'
+                                          placeholder='Price ($ per person)'
+                                          // value={props.price}
+                                          // onChange={props.priceChange}
+                                          maxLength={3} pattern='[0-9]*'
+                                          />
+                                      </label>
+
+                                      <label htmlFor='minimumPlayers'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='minimumPlayers'
+                                          id='minimumPlayers'
+                                          type='text'
+                                          placeholder='Minimum players'
+                                          // value={props.minimumPlayers}
+                                          // onChange={props.minimumPlayersChange}
+                                          maxLength={2} />
+                                      </label>
+
+                                      <label htmlFor='difficulty'>
+                                        <select
+                                          required
+                                          className='dropdown'
+                                          name='difficulty'
+                                          id='difficulty'
+                                          // onClick={props.difficultyChange}
+                                          >
+                                          <option value=''>Select a difficulty here</option>
+                                          <option value='Easy'>Easy</option>
+                                          <option value='Medium'>Medium</option>
+                                          <option value='Hard'>Hard</option>
+                                        </select>
+                                      </label>
+
+                                      <label htmlFor='timeLimit'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='timeLimit' id='timeLimit'
+                                          type='text'
+                                          placeholder='Time limit (minutes)'
+                                          // value={props.timeLimit}
+                                          // onChange={props.timeLimitChange}
+                                          />
+                                      </label>
+
+                                      <label htmlFor='phoneNumber'>
+                                        <input
+                                          className='new-listing-input'
+                                          name='phoneNumber'
+                                          id='phoneNumber'
+                                          type='text'
+                                          placeholder='Phone number'
+                                          // value={props.phoneNumber}
+                                          // onChange={props.phoneNumberChange}
+                                          maxLength={10}
+                                          />
+                                      </label>
+
+                                      <label htmlFor='description'>
+                                        <textarea
+                                          className='new-listin-desc'
+                                          name='description' id='description'
+                                          placeholder='Description'
+                                          // value={props.description}
+                                          // onChange={props.descriptionChange}
+                                           />
+                                      </label>
+
+                                      <div className='mb-4'>
+                                        <button className='new-listing-submit' type='submit' style={{
+                                          cursor: 'pointer'
+                                        }}>Submit</button>
+                                      </div>
+
+                                    </div>
+
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+
                           </div>
                         </div>
                         )
@@ -287,21 +443,31 @@ export default class VendorHome extends React.Component {
                   }
 
                   {/* TESTING CONDITIONAL INFO ABOVE BUTTON */}
-                  <div className='d-flex justify-content-center mt-3 mb-3 add-listing'>
-                    <button style={{
-                      backgroundColor: '#1976D2',
-                      color: '#fff',
-                      borderRadius: '5rem',
-                      width: '7.5rem',
-                      textAlign: 'center',
-                      paddingTop: '.25rem',
-                      cursor: 'pointer',
-                      outline: 'none',
-                      borderStyle: 'none'
-                    }} onClick={this.openForm}>
-                      Add a listing
-                    </button>
-                  </div>
+                  {
+                    (this.state.editClicked === false)
+                      ? (
+                        <div className='d-flex justify-content-center mt-3 mb-3 add-listing'>
+                          <button style={{
+                            backgroundColor: '#1976D2',
+                            color: '#fff',
+                            borderRadius: '5rem',
+                            width: '7.5rem',
+                            textAlign: 'center',
+                            paddingTop: '.25rem',
+                            cursor: 'pointer',
+                            outline: 'none',
+                            borderStyle: 'none'
+                          }} onClick={this.openForm}>
+                            Add a listing
+                          </button>
+                        </div>
+                        )
+                      : (
+                        <>
+                        &nbsp;
+                        </>
+                        )
+                  }
 
                 </div>
 
