3c74850c22b3bb090b1e762d5db4771e508d046f
Matthew Chau <matthewwchau@gmail.com>
2023-07-27 05:28:30 +0000

    Update the section on how to use this site in the accordion component. #37

--- client/components/accordion.jsx
+++ client/components/accordion.jsx
@@ -61,11 +61,10 @@ export default function HomeAccordion(props) {
           <AccordionDetails>
             <Typography>
 
-              An escape room is a game in which a team of players discover clues,
+              An escape room is a group puzzle game in which a team of players discovers clues,
               solve puzzles, and accomplish tasks in one or more
               rooms in order to accomplish a specific goal in a limited amount of time. The goal is
-              often to escape from the site of the game.&nbsp;
-              <a className='wiki' href='https://en.wikipedia.org/wiki/Escape_room' target="_blank" rel="noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Learn more here!</a>
+              often to complete all rooms and escape the room. <a className='wiki' href='https://en.wikipedia.org/wiki/Escape_room' target="_blank" rel="noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Learn more here!</a>
 
             </Typography>
           </AccordionDetails>
@@ -76,12 +75,41 @@ export default function HomeAccordion(props) {
             <Typography>How to use this site?</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography style={{ color: 'red' }} >
+            <Typography>
 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-              sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-              sit amet blandit leo lobortis eget.
+              <b>Participants:</b> If you are a new player, you can find the online catalog by navigating to the &quot;Participants&quot; page.
+              To do so, you can route to the page by clicking the blue &quot;Participants&quot; button above this instruction.
+              You may also find the &quot;Participants&quot; button in the nav bar. The nav bar can be accessed by clicking on the menu icon on the top left corner of the page.
+
+            </Typography>
+            <Typography mt={1}>
+
+              Once you are on the catalog page, you can find a list of rooms that were listed by vendors. To learn more about a listing, click on the title of the room,
+              and it will direct you to another page with more information. If you are interested in booking a room with the vendor, you may contact them with the phone number provided.
+              Before booking a room, be sure to gather a group size that fits the requirement and find a theme your party will enjoy!
+
+            </Typography>
+            <Typography mt={1}>
+
+              Lastly, make sure to communicate within the team! Communication is key!
+
+            </Typography>
+            <Typography mt={2}>
+
+              <b>Vendors:</b> If you are a vendor and have an escape room you would like to post a listing for, sign in to your account and add the room on the vendor portal.
+              First, click on the button labeled &quot;Vendor&quot; above these instructions OR access the &quot;Vendor&quot;
+              If you currently do not have an account, you can follow the prompts to create an account.
+              If you already have an account, you may click on the link below the form to sign in.
+
+            </Typography>
+            <Typography mt={1}>
+
+              Once you&apos;ve signed in, you may click on the button labeled &quot;Add a listing&quot; and follow the prompts to create a listing.
+
+            </Typography>
+            <Typography mt={1}>
+
+              Just like that, you&apos;ve successfully created a listing!
 
             </Typography>
           </AccordionDetails>
@@ -95,9 +123,11 @@ export default function HomeAccordion(props) {
             <Typography>
 
               If you are a vendor and would like to add your room as a listing on this site, create an account
-              to add your listing to our catalog! Click on this link&nbsp;
-              <a className='new-vendor' style={{ textDecoration: 'underline' }} onClick={props.routeVSignup}>here</a> to get started!
+              to add your listing to our catalog!
 
+            </Typography>
+            <Typography mt={2}>
+              Click on this link <a className='new-vendor' style={{ textDecoration: 'underline' }} onClick={props.routeVSignup}>here</a> to get started!
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -125,11 +155,13 @@ export default function HomeAccordion(props) {
 
               This site was created out of a hobby for escape rooms. Hopefully it can be yours too!
               There are many types and themes of escape rooms out there, find your first room here!
+
+            </Typography>
+            <Typography mt={2}>
               Click <a
                 className='about-us-a'
                 style={{ textDecoration: 'underline', color: 'inherit' }}
                 href='https://github.com/MattChau01/escape-room' target="_blank" rel="noreferrer">here</a> for a link to the project repository on GitHub.
-
             </Typography>
           </AccordionDetails>
         </Accordion>
